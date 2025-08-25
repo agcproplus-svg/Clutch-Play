95bac6469eccf7db03c25cba0aaa6cb7e4b82ff2
agcproplus-svg <agcproplus@gmail.com>
2025-08-25 15:34:23 -0400

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,4 @@
-document.getElementById('app').innerHTML = '<h1>Gridiron Cards 2008 - Placeholder Build</h1>';
+document.getElementById('app').innerHTML = `
+  <h1>Gridiron 2008 Pro</h1>
+  <p>Starter build loaded. UI, team select, and play system will render here.</p>
+`;
